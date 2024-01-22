54f57be145e50781f66076cb24e14e9b0043cfc9
Robert Roden <110652096+RobertRoden@users.noreply.github.com>
2024-01-21 21:40:50 -0600

    Add files via upload

---
+++ testscorearrays.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int testScores[5]{ 100, 95, 99, 87, 88 };
+
+
+	cout << "Enter the first test score:";
+		cin >> testScores[0];
+
+		cout << "Enter the second test score:";
+			cin >> testScores[1];
+
+			cout << "Enter the third test score:";
+			cin >> testScores[2];
+
+				cout << "Enter the fourth test score:";
+				cin >> testScores[3];
+
+				cout << "Enter the fifth test score:";
+				cin >> testScores[4];
+
+			testScores[0] = 90;
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
