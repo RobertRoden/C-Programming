6e5e7fcbe5720843cb98e3b221f9cd8b24230598
Robert Roden <110652096+RobertRoden@users.noreply.github.com>
2024-01-21 21:28:17 -0600

    Add files via upload

---
+++ ballHeight.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+	string country;
+	
+	cout << "Where do you live? ";
+	cin >> country;
+	
+	cout << "I heard great things about " << country << " I'd like to go sometime" << endl;
+
+
+	return 0;
+}
